9fb0166415e6e68f7b987ada03144667e355e19a
kiet-na <166475992+kiet-na@users.noreply.github.com>
2024-12-07 20:31:00 +0700

    code: commit

--- simple_client.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('127.0.0.1', 6881))
-client_socket.sendall(b"Hello, Server!")
-data = client_socket.recv(1024)
-print(f"Received data: {data}")
-client_socket.close()
